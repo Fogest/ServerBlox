8cd09368cac44fe48acf4c3c5b9a37bb93502751
Marvin Bek <delocaz@gmail.com>
2012-09-26 12:55:44 +0200

    Back to the old lang loading
    Also added shell script for counting lines of code

--- src/me/Delocaz/ServerBlox/SBLang.java
+++ src/me/Delocaz/ServerBlox/SBLang.java
@@ -1,29 +1,16 @@
 package me.Delocaz.ServerBlox;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
 
 public class SBLang {                                                    
 	public SBConfig lngf;                                                
 	public HashMap<String, String> lng = new HashMap<String, String>();
 	public SBLang() {                                                    
 		lngf = new SBConfig("lang");
-		YamlConfiguration df = new YamlConfiguration();
-		try {
-			df.loadFromString(Files.toString(new File(getClass().getResource("lang.yml").getFile()), Charsets.UTF_8));
-		} catch (InvalidConfigurationException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		lngf.addDefaults(df);
+		lngf.addDefaults(YamlConfiguration.loadConfiguration(getClass().getResourceAsStream("/lang.yml")));
 		lngf.options().copyDefaults(true);
 		lngf.save();
 		int i = 0;
